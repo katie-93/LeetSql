b7364eba8e1c5671ea341f67c0d8aa94df1a84c1
Ninajkjk <103212602+Ninajkjk@users.noreply.github.com>
2024-01-06 15:43:01 -0600

    1 modification

--- leet-1193.sql
+++ leet-1193.sql
@@ -9,7 +9,7 @@ SELECT
 FROM
 	(
 	SELECT
-		date_format( trans_date,’% Y -% m’ ) AS MONTH,
+		date_format( trans_date,'% Y -% m' ) AS MONTH, --edition
 		Country,
 		count( Country ) AS trans_count,
 		sum( amount ) AS trans_total_amount 
@@ -17,4 +17,4 @@ FROM
 	TRANSACTION 
 	GROUP BY
 	( MONTH, Country ) AS a
-	LEFT JOIN ( SELECT country, state, count( country ) AS approved_count, sum( amount ) AS approved_total_amount, trans_date FROM TRANSACTION WHERE state =‘ approved’ ) AS b ON a.country = b.country
+	LEFT JOIN ( SELECT country, state, count( country ) AS approved_count, sum( amount ) AS approved_total_amount, trans_date FROM TRANSACTION WHERE state =‘ approved’ )) AS b ON a.country = b.country
